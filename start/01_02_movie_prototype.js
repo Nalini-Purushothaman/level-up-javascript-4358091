06a6ea03ad8e2c3052da64ce7a131633f3c68b11
Nalini-Purushothaman <152356215+Nalini-Purushothaman@users.noreply.github.com>
2024-01-30 19:43:44 +0000

    Movie exercise commit

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,17 @@
-// Write your code here
+class Movie {
+  constructor (title, director, genre, releaseYear, rating){
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+  getOverview(){
+    const overviewMsg = `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
+    return overviewMsg;
+  }
+}
+
+const newMovie = new Movie("Titanic","directorTitanic","Romance",2000,10);
+console.log(newMovie.getOverview());
+
