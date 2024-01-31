05c0466def30acae52c3e8acd3fc6b30fc269b82
Nalini-Purushothaman <152356215+Nalini-Purushothaman@users.noreply.github.com>
2024-01-31 00:24:19 +0000

    symbol exercise commit

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,14 @@
 // Write your code here
+const password1 = Symbol("password");
+const user = {
+  username : "abc@gmail.com",
+  [password1] : "1234566",
+  //passwordMatch : "1234566"
+}
+console.log("User Name:",user.username);
+console.log("Password",user.password1);
+// console.log("Password Match",user.passwordMatch);
+// if(user.password1===user.passwordMatch)
+//   console.log("Password Matches");
+// else
+//   console.log("Password doesnt match");
