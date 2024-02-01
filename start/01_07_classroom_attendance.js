e29f61a1957557e34b1baec27d18a54a885c4afe
Nalini-Purushothaman <152356215+Nalini-Purushothaman@users.noreply.github.com>
2024-02-01 06:13:50 +0000

    destructive operator exercise commit

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,16 @@
 // Write your code here
+const classroom1 = {
+  hasTeachingAssistant : true,
+  classList : ["teacher1","ATeacher1","Student1","Student2","Student3","Student4"],
+}
+const classroom2 = {
+  hasTeachingAssistant : false,
+  classList : ["teacher1","Student1","Student2","Student3","Student4"],
+}
+function getStudents(classroom){
+  if(classroom.hasTeachingAssistant === true)
+  {
+    console.log("has teaching assistant");
+  }
+
+}
