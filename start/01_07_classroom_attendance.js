b2eaafa5552c96359f982129bb480f278a252295
Nalini-Purushothaman <152356215+Nalini-Purushothaman@users.noreply.github.com>
2024-02-01 08:01:11 +0000

    destructive objects and arrays solution

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,16 +1,31 @@
 // Write your code here
 const classroom1 = {
-  hasTeachingAssistant : true,
-  classList : ["teacher1","ATeacher1","Student1","Student2","Student3","Student4"],
-}
-const classroom2 = {
-  hasTeachingAssistant : false,
-  classList : ["teacher1","Student1","Student2","Student3","Student4"],
-}
-function getStudents(classroom){
-  if(classroom.hasTeachingAssistant === true)
-  {
-    console.log("has teaching assistant");
+    hasTeachingAssistant : true,
+    classList : ["teacher1","ATeacher1","Student1","Student2","Student3","Student4"],
   }
-
-}
+  const classroom2 = {
+    hasTeachingAssistant : false,
+    classList : ["teacher1","Student1","Student2","Student3","Student4"],
+  }
+  function getStudents(classroom){
+    const {hasTeachingAssistant,c2}=classroom;
+    console.log(hasTeachingAssistant);
+    console.log(c2);
+    if(classroom.hasTeachingAssistant === true)
+    {
+      console.log("has teaching assistant");
+      const [v1,v2,...v3]=classroom.classList;
+      console.log("v1:",v1);
+      console.log("v2:",v2);
+      console.log("v3:",v3);
+    }
+    else{
+      const [v1,...v2]=classroom.classList;
+      console.log("v1:",v1);
+      console.log("v2:",v2);
+    }
+  }
+  console.log("classroom 1 with assistant");
+  getStudents(classroom1);
+  console.log("classroom 2 without assistant");
+  getStudents(classroom2);
