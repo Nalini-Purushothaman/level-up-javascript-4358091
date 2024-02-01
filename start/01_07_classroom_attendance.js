c033a242968b79dbf1a7ddd3c01f4b01c2cd8fe5
Nalini-Purushothaman <152356215+Nalini-Purushothaman@users.noreply.github.com>
2024-02-01 21:52:15 +0000

    error fixed

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,4 +1,3 @@
-// Write your code here
 const classroom1 = {
     hasTeachingAssistant : true,
     classList : ["teacher1","ATeacher1","Student1","Student2","Student3","Student4"],
@@ -8,19 +7,19 @@ const classroom1 = {
     classList : ["teacher1","Student1","Student2","Student3","Student4"],
   }
   function getStudents(classroom){
-    const {hasTeachingAssistant,c2}=classroom;
+    const {hasTeachingAssistant,classList}=classroom;
     console.log(hasTeachingAssistant);
-    console.log(c2);
+    console.log(classList);
     if(classroom.hasTeachingAssistant === true)
     {
       console.log("has teaching assistant");
-      const [v1,v2,...v3]=classroom.classList;
+      const [v1,v2,...v3]=classList;
       console.log("v1:",v1);
       console.log("v2:",v2);
       console.log("v3:",v3);
     }
     else{
-      const [v1,...v2]=classroom.classList;
+      const [v1,...v2]=classList;
       console.log("v1:",v1);
       console.log("v2:",v2);
     }
