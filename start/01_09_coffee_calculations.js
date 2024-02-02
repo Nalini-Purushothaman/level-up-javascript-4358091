3f7ff8cd9ec8e299176d69b7763ece9dad2dfc10
Nalini-Purushothaman <152356215+Nalini-Purushothaman@users.noreply.github.com>
2024-02-02 01:52:20 +0000

    array reduce method exercise

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,11 @@
 // Write your code here
+const coffee = [2,3,1,4,3,2];
+// array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+const totalCoffeeCount = coffee.reduce(calcBill);
+console.log("totalCoffeeCount:",totalCoffeeCount);
+const totalBillAmount = totalCoffeeCount * 1.25;
+console.log("totalBillAmount:",totalBillAmount);
+
+function calcBill(total1, currentValue){
+  return total1 = total1 + currentValue; 
+}
