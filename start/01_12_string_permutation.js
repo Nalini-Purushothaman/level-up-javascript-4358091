e23deaa2db461fdda2445f77bab33f9217e4f1ac
Nalini-Purushothaman <152356215+Nalini-Purushothaman@users.noreply.github.com>
2024-02-02 07:40:05 +0000

    recursive function exercise

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,17 @@
 // Write your code here
+let permString = 1;
+function getPermutation(length){
+  permString *= length;
+  //console.log(permString);
+  length--;
+  if(length === 1){
+    console.log("Word scrabble permutation of the word ",string, "is ",permString);
+    return;
+  }
+  else
+    getPermutation(length);
+}
+
+const string = "game";
+const strLength = string.length;
+getPermutation(strLength);
