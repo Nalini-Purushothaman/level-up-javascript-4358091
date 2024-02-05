6d2286fe465f2e6da54cd03953e752b0ced56914
Nalini-Purushothaman <152356215+Nalini-Purushothaman@users.noreply.github.com>
2024-02-05 08:20:46 +0000

    regular expressions exercise

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,7 @@
 // Write your code here
+const blogPostString = "Em ma's bl/og!";
+let urlifiedString = blogPostString.toLocaleLowerCase();
+urlifiedString1 = urlifiedString.replace(/\s/g, '-');
+urlifiedString2 = urlifiedString1.replace(/[!@#$%^&*'/]/g, "");
+console.log("Original String : ",blogPostString);
+console.log("Output String : ",urlifiedString2);
