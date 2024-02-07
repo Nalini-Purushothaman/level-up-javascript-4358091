3316b9754d1a17fdbe231283e0ef738870c41952
Nalini-Purushothaman <152356215+Nalini-Purushothaman@users.noreply.github.com>
2024-02-07 05:36:42 +0000

    regex password strength exercise

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,8 @@
 // Write your code here
+
+let text = "01234567a8901A23456789";
+let text1 = "asdfafdassafasfsa";
+let pattern = /(?=.+[0-9])(?=.+[a-z])(?=.+[A-Z])(?=.{8,})/; 
+let result = pattern.test(text);
+result ? document.getElementById("demo").innerHTML = "true":document.getElementById("demo").innerHTML = "false";
+//document.getElementById("demo").innerHTML = result;
